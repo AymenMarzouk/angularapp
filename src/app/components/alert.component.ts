0143b63c23883453767dfd5855b9ad6a025ad49a
mahraneAZERTY <chabaanemahrane985@gmail.com>
2020-08-20 17:26:56 +0100

    style css

--- src/app/components/alert.component.ts
+++ src/app/components/alert.component.ts
@@ -4,9 +4,10 @@ import { Subscription } from 'rxjs';
 import { AlertService } from '../services';
 
 @Component({
-    selector: 'alert',
-    templateUrl: 'alert.component.html'
-})
+    selector: 'app-alert',
+    templateUrl: './alert.component.html',
+    styleUrls: ['./alert.component.css']
+  })
 
 export class AlertComponent implements OnInit, OnDestroy {
     private subscription: Subscription;
